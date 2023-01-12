5b5e8841839e45fb6b560e38d8c51cf70e8ebe28
IgorSilvaJ <igorjsilvabiz@gmail.com>
2023-01-11 21:42:47 -0300

    Adiciona header com as informações da pessoa jogadora

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userImg } from '../APIs/fetch';
 
-export default class Game extends Component {
+class Game extends Component {
   render() {
+    const { name, email } = this.props;
     return (
-      <div>Game</div>
+      <div>
+        <img
+          src={ userImg(email) }
+          alt="Placeholder"
+          data-testid="header-profile-picture"
+        />
+        <span data-testid="header-player-name">{name}</span>
+        <span data-testid="header-score">0</span>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  name: state.loginReducer.name,
+  email: state.loginReducer.email,
+});
+
+Game.propTypes = {
+  name: PropTypes.string,
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Game);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,16 +1,14 @@
-import { requestTokenTrivia } from '../../APIs/fetch';
+import { getToken } from '../../APIs/fetch';
 
 export const LOGIN_ACTION = 'LOGIN_ACTION';
 export const REQUEST_TOKEN = 'REQUEST_TOKEN';
 
-export const login = () => ({
+export const login = (name, email, img) => ({
   type: LOGIN_ACTION,
+  name,
+  email,
+  img,
 });
-
-const getToken = async () => {
-  const data = await requestTokenTrivia();
-  return data;
-};
 
 export const requestToken = () => ({
   type: REQUEST_TOKEN,
