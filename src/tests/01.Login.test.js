73933995db590347b012be336757636319127f5d
IgorSilvaJ <igorjsilvabiz@gmail.com>
2023-01-11 22:18:48 -0300

    Corrige teste da pagina Login quando redireciona para /game

--- src/tests/01.Login.test.js
+++ src/tests/01.Login.test.js
@@ -3,10 +3,18 @@ import Login from "../pages/Login";
 import userEvent from "@testing-library/user-event";
 import { screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import App from "../App";
+import { act } from "react-dom/test-utils";
 
 const mockData = '3ae74acd9b3e9db480abce3be612b84b54e85f0482b0448059ca764939849236';
 
 describe('Test Login component', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch');
+      global.fetch.mockResolvedValue({
+        json: jest.fn().mockResolvedValue(mockData),
+      });
+  });
+
   it('should be possible to write the user name in the correct field', () => {
     renderWithRouterAndRedux(<Login />);
     const email = screen.getByRole('textbox', { name: /email:/i });
@@ -14,35 +22,26 @@ describe('Test Login component', () => {
     userEvent.type(email, 'Arthur@betrybe.com');
     expect(email.value).toBe('Arthur@betrybe.com');
   });
+
   it('should redirect to game page', async () => {
-    // jest.spyOn(global, 'fetch');
-    // global.fetch.mockResolvedValue({
-    //   json: jest.fn().mockResolvedValue(mockData),
-    // });
-
-    jest.spyOn(global, 'fetch').mockResolvedValue({
-      json: jest.fn().mockResolvedValue(mockData)
-    })
-
     const { history } = renderWithRouterAndRedux(<App />);
     const btnPlay = screen.getByRole('button', { name: /play/i });
     expect(btnPlay).toBeDisabled();
-
     const email = screen.getByRole('textbox', { name: /email:/i });
     const name = screen.getByRole('textbox', { name: /nome:/i });
     expect(email).toBeInTheDocument();
     expect(name).toBeInTheDocument();
-
+    
+    
     userEvent.type(email, 'Arthur@betrybe.com');
     userEvent.type(name, 'Arthur');
-
     expect(btnPlay).toBeEnabled();
-
     userEvent.click(btnPlay);
 
-    // await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1))
-    const { pathname } = history.location;
-    expect(pathname).toBe('/game')
+    await waitFor(() => expect(history.length).toBe(2))
+    await act( async () => {
+      expect(history.location.pathname).toBe('/game')
+    })
   });
 
   it('should redirect to game settings page', async () => {
@@ -50,7 +49,6 @@ describe('Test Login component', () => {
     
     const btnConfig = screen.getByRole('button', {  name: /configurações/i})
     userEvent.click(btnConfig);
-
     const { pathname } = history.location;
     expect(pathname).toBe('/settings')
   });
