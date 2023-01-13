9b4924b5823ac8cceb29c5f76cb91595c80af413
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2023-01-13 20:41:57 -0300

    header da tela de feedback implementado

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userImg } from '../APIs/fetch';
 
 class Feedback extends Component {
+  feedbackResult = () => {
+  };
+
   render() {
+    const { name, gravatarEmail, score } = this.props;
     return (
-      <div data-testid="feedback-text">Feedback</div>
+      <>
+        <div data-testid="feedback-text">Feedback</div>
+        <img
+          data-testid="header-profile-picture"
+          src={ userImg(gravatarEmail) }
+          alt={ `Foto da pessoa usuária: ${name}` }
+        />
+        <span
+          data-testid="header-player-name"
+        >
+          { name }
+        </span>
+        <p>
+          Score:
+          <span
+            data-testid="header-score"
+          >
+            { score }
+          </span>
+        </p>
+        <span data-testid="feedback-text">{ this.feedbackResult }</span>
+      </>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  name: state.loginReducer.name,
+  gravatarEmail: state.loginReducer.gravatarEmail,
+  score: state.player.score,
+  assertions: state.player.assertions,
+});
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+  score: PropTypes.number,
+  name: PropTypes.string,
+  gravatarEmail: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
