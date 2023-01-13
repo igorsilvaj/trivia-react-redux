8bf6760e7b0757afd75a0c50e658f0687f9edcfd
IgorSilvaJ <igorjsilvabiz@gmail.com>
2023-01-13 12:38:06 -0300

    Corrige click nos botões mantendo a mesma posição ao trocar a classe

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,16 +3,16 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { userImg, getQuestions } from '../APIs/fetch';
 import { Loading } from '../components/Loading';
-import shuffle from '../helpers/shuffleArray';
-import gameAnswerPushHelper from '../helpers/gameAnswerPushHelper';
+import GameQuestions from '../components/GameQuestions';
+import shuffleArray from '../helpers/shuffleArray';
 
 class Game extends Component {
   state = {
     questions: [],
     currentQuestion: 0,
     isLoading: true,
-    disabledColorWrong: '',
     disabledColorCorrect: '',
+    disabledColorIncorrect: '',
   };
 
   async componentDidMount() {
@@ -26,18 +26,34 @@ class Game extends Component {
     }
   }
 
-  handleClick() {
-    // target.classList.add(target.name === 'incorrect' ? 'incorrect' : 'correct');
-    console.log('test');
-    this.setState({ disabledColorWrong: 'incorrect', disabledColorCorrect: 'correct' });
+  nextQuestion() {
+    const { currentQuestion, questions } = this.state;
+    if (currentQuestion === questions.length - 1) {
+      console.log('last question');
+    } else {
+      this.setState({
+        currentQuestion: currentQuestion + 1,
+      });
+    }
   }
 
   render() {
     const { name, email } = this.props;
-    const { questions, currentQuestion, isLoading,
-      disabledColorWrong, disabledColorCorrect } = this.state;
-    let answerMap = [];
-    const lastNuber = 0;
+    const {
+      questions,
+      currentQuestion,
+      isLoading,
+      disabledColorIncorrect,
+      disabledColorCorrect,
+    } = this.state;
+    let rAnswer = [];
+    if (!isLoading) {
+      const correctA = {
+        text: questions[currentQuestion].correct_answer, type: 'correct' };
+      const wrongA = questions[currentQuestion].incorrect_answers.map((e) => (
+        { text: e, type: 'incorrect' }));
+      rAnswer = shuffleArray([correctA, ...wrongA]);
+    }
     return (
       <div>
         <img
@@ -46,54 +62,27 @@ class Game extends Component {
           data-testid="header-profile-picture"
         />
         <span data-testid="header-player-name">{name}</span>
-        <span data-testid="header-score">0</span>
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <div className="questionContainer">
-            <p data-testid="question-category">
-              {questions[currentQuestion].category}
-            </p>
-            <p data-testid="question-text">
-              {questions[currentQuestion].question}
-            </p>
-            <div data-testid="answer-options" className="answerContainer">
-              {gameAnswerPushHelper(
-                answerMap,
-                questions[currentQuestion].incorrect_answers,
-                questions[currentQuestion].correct_answer,
-              )}
-              {shuffle(answerMap)}
-              {answerMap.map((e, index) => {
-                const random = Math.floor(Math.random() * answerMap.length);
-                const randomTest = random === lastNuber
-                  ? Math.floor(Math.random() * answerMap.length)
-                  : random;
-                const answer = answerMap[randomTest];
-                answerMap = answerMap.filter((el) => el.text !== answer.text);
-                return (
-                  <button
-                    type="button"
-                    name={ e.type }
-                    data-testid={
-                      e.type === 'incorrect'
-                        ? `wrong-answer-${index}`
-                        : 'correct-answer'
-                    }
-                    key={ `${index}-question` }
-                    onClick={ () => this.handleClick() }
-                    className={ e.type === 'incorrect'
-                      ? disabledColorWrong
-                      : disabledColorCorrect }
-                  >
-                    {e.text}
-                  </button>
-                );
-              })}
-            </div>
-            <button type="button">Próxima pergunta</button>
-          </div>
-        )}
+        <p>
+          Score:
+          <span data-testid="header-score">{' 0'}</span>
+        </p>
+        {
+          isLoading ? (<Loading />) : (
+            <>
+              <GameQuestions
+                key={ currentQuestion }
+                question={ questions[currentQuestion] }
+                answers={ rAnswer }
+                handleQuestionClick={ () => this.handleQuestionClick() }
+                correct={ disabledColorCorrect }
+                incorrect={ disabledColorIncorrect }
+              />
+              <button type="button" onClick={ () => this.nextQuestion() }>
+                Próxima pergunta
+              </button>
+            </>
+          )
+        }
       </div>
     );
   }
