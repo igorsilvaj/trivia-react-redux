6f62e1540695ba6d499577f5876e381488097347
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2023-01-13 20:23:25 -0300

    adiciona a pagina de feedback e o redirecionamento da mesma

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Game from './pages/Game';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route exact path="/" component={ Login } />
         <Route path="/settings" component={ Settings } />
         <Route path="/game" component={ Game } />
+        <Route path="/feedback" component={ Feedback } />
       </Switch>
     </div>
   );
