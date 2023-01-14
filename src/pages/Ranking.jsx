ce776a535deb528e679d1bf20a2ada923c346694
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2023-01-13 21:21:24 -0300

    botao para ir a pagina inicial implementado

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <>
+        <div data-testid="ranking-title">Ranking</div>
+        <button
+          type="button"
+          onClick={ () => history.push('/') }
+          data-testid="btn-go-home"
+        >
+          Ranking
+        </button>
+      </>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.func,
+}.isRequired;
